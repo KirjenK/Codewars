9894b2e69c6a1643c56ee6873d231bc7028098f2
Kirill <kirpitch@inbox.ru>
2022-08-21 00:30:38 +0300

    codewars

--- 5kyu/Moving-Zeros-To-The-End.js
+++ 5kyu/Moving-Zeros-To-The-End.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/52597aa56021e91c93000cb0
+// https://www.codewars.com/kata/52597aa56021e91c93000cb0
 
 // My solution
 
--- 6kyu/Convert-String-To-Camel-Case.js
+++ 6kyu/Convert-String-To-Camel-Case.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/517abf86da9663f1d2000003
+// https://www.codewars.com/kata/517abf86da9663f1d2000003
 
 // My solution
 
