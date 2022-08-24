950f4d97dae3f1f024ee1e670d8cb59c3d60d4fd
Kirill <kirpitch@inbox.ru>
2022-08-25 02:14:00 +0300

    add

--- 6kyu/Find the total white and black areas in a strange chessboard.js
+++ 6kyu/Find the total white and black areas in a strange chessboard.js
@@ -20,3 +20,38 @@ function whiteBlackAreas(cols, rows) {
       }
     return [(csWhite * rsWhite) + (csBlack * rsBlack), (rsBlack * csWhite) + (csBlack * rsWhite)]
     }
+
+    // Cycle hypnosis // 
+
+    function whiteBlackAreas(cols, rows) {
+        let white = []
+        let black = []
+        for(let i = 0; i < cols.length; i = i + 2) {
+          for(let j = 0; j < rows.length; j = j + 2) {
+            white.push(cols[i] * rows[j])
+          }
+        }
+        for(let i = 1; i < rows.length; i = i + 2) {
+          for(let j = 1; j < cols.length; j = j + 2) {
+           white.push(rows[i] * cols[j])
+          }
+        }
+        
+        for(let i = 0; i < cols.length; i = i + 2 ) {
+          for(let j = 1; j < rows.length; j = j + 2) {
+                black.push(rows[j] * cols[i])
+          }
+        }
+        for(let i = 1; i < cols.length; i = i + 2 ) {
+          for(let j = 0; j < rows.length; j = j + 2) {
+                black.push(rows[j] * cols[i])
+          }
+        }
+        
+        let resultWhite = white.reduce((el, sum) => el + sum, 0)
+        let resultBlack = black.reduce((el, sum) => el + sum, 0)
+      
+      return [resultWhite, resultBlack]
+      }
+      
+      console.log(whiteBlackAreas([3,1,2,7,1],[1,8,4,5,2,]))
