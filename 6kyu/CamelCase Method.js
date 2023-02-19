58817e5e7c831399f7b5d3f5332f9f001d2c7aaa
Kirill <kirpitch@inbox.ru>
2023-02-19 19:14:01 +0300

    CamelCase Method prototype

---
+++ 6kyu/CamelCase Method.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/587731fda577b3d1b0001196/javascript
+
+// My solution
+
+String.prototype.camelCase = function () {
+  const arrOfStrings = this.split(' ');
+  const result = arrOfStrings
+    .map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+    .join('');
+  return result;
+};
