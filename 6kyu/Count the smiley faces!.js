7b2f09db65a2922a2f5e2bb9c70a95dcb6be48c3
Kirill <kirpitch@inbox.ru>
2023-03-21 13:45:27 +0300

    refactoring

--- 6kyu/Count the smiley faces!.js
+++ 6kyu/Count the smiley faces!.js
@@ -4,21 +4,18 @@ function countSmileys(arr) {
   const smileyFacesWithoutNose = ':);):D;D';
   const smileyFacesWithNose = ':-):~);-);~):-D:~D;-D;~D';
   let counter = 0;
-  let checkStr = '';
-  arr.forEach((str) => {
-    for (let i = 0; i < str.length; i += 1) {
-      const char = str[i];
-      checkStr += char;
-    }
-    const checkFaceNoNose = checkStr.slice(checkStr.length - 2);
-    const checkFaceWithNose = checkStr.slice(checkStr.length - 3);
+  arr.forEach((el) => {
+    const checkFaceNoNose = el.slice(el.length - 2);
+    const checkFaceWithNose = el.slice(el.length - 3);
 
     if (smileyFacesWithoutNose.includes(checkFaceNoNose) && checkFaceNoNose.length > 1) {
       counter += 1;
     } else if (smileyFacesWithNose.includes(checkFaceWithNose) && checkFaceWithNose.length > 1) {
       counter += 1;
     }
-    checkStr = '';
   });
   return counter;
 }
+
+const res = countSmileys([';]', ':[', ';*', ':$', ';-D']);
+console.log(res);
